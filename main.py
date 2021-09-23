7a28c316ad5b0fcb462dc6c83c62b66457f0725f
Dulan Jayasuriya <dulanjayasuriya@gmail.com>
2021-09-23 11:38:08 +0530

    Get image and save

--- main.py
+++ main.py
@@ -4,10 +4,13 @@
 @Author:      dulanj
 @Time:        2021-09-23 11.22
 """
-
 from typing import Optional
 
+import cv2
+import numpy as np
+from fastapi import Body
 from fastapi import FastAPI
+from pydantic import Required
 
 app = FastAPI()
 
@@ -20,3 +23,10 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.post("/image/predict")
+def authenticate(image_byte_stream: bytes = Body(Required, media_type="application/octet-stream")):
+    img = cv2.imdecode(np.frombuffer(image_byte_stream, np.uint8), -1)
+    cv2.imwrite("test.jpg", img)
+    return {"status_code": "Image saved", "status": "200"}
